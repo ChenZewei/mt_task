29658427720bbf9871612173f7275ed511f6fa92
Zewei Chen <czwking1991@gmail.com>
2020-02-14 16:33:53 +0800

    add script

---
+++ test.sh
@@ -0,0 +1,4 @@
+./mt_task -e 10 -p 100 -d 100 -m 8 -t 10 -P 0
+./mt_task -e 10 -p 100 -d 100 -m 8 -t 10 -P 1
+./mt_task -e 10 -p 100 -d 100 -m 8 -t 10 -P 2
+./mt_task -e 10 -p 100 -d 100 -m 8 -t 10 -P 3
