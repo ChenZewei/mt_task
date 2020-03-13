a0dcd4e35b3d42af25c90b2c8bfc716fc8762af1
Zewei Chen <czwking1991@gmail.com>
2020-03-13 12:25:58 -0400

    --

--- src/mt_task.c
+++ src/mt_task.c
@@ -308,14 +308,14 @@ int job(void) {
 
 void loop_ms(long ms) {
 	long n = 0;
-	long iteration = ms * 300000;
+	long iteration = ms * 267000;
 	while (++n < iteration) {}
 	return NULL;
 }
 
 void loop_us(long us) {
 	long n = 0;
-	long iteration = us * 300;
+	long iteration = us * 267;
 	while (++n < iteration) {}
 	return NULL;
 }
