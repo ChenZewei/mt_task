6c1ac0ab888e16a45c6f01c26876a35b1cc629f9
Zewei Chen <czwking1991@gmail.com>
2020-03-20 18:36:59 -0400

    --

--- litmus-scripts/gedf_st.sh
+++ litmus-scripts/gedf_st.sh
@@ -16,7 +16,7 @@ for norm_u in $(seq 0.1 0.1 1)
 do
   for((i=1;i<=$iteration;i++));
   do
-    ./tasksets/global_$(m)_$(p)_$(duration)_$(i)_$(norm_u).sh
+    ./tasksets/global_${m}_${p}_${duration}_${i}_${norm_u}.sh
     release_ts
   done
 done
