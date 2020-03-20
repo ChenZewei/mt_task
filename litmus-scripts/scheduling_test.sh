586e16d9664b37bb73cc3a53f01db2404101ee0a
Zewei Chen <czwking1991@gmail.com>
2020-03-19 23:44:02 -0400

    --

--- litmus-scripts/scheduling_test.sh
+++ litmus-scripts/scheduling_test.sh
@@ -8,6 +8,9 @@ cd $EXP_DIR
 
 python task_gen_vary_utilization.py $1 $2 $3 $5
 
+gap=`expr 5 + $3 / 1000`
+echo $gap
+
 cd $ST_DIR
 cd $ST_DIR/gedf
 ./clean.sh
@@ -16,7 +19,7 @@ cd $EXP_DIR
 ./global_test.sh &
 sleep 5
 cd $ST_DIR/gedf
-sleep `expr 5 + $3/1000` | st-trace-schedule gedf_m$1_p$2_d$3_$4_u$5 &
+sleep $gap | st-trace-schedule gedf_m$1_p$2_d$3_$4_u$5 &
 release_ts &
 wait
 ./st.sh gedf_m$1_p$2_d$3_$4_u$5
@@ -29,7 +32,7 @@ cd $EXP_DIR
 ./global_test.sh &
 sleep 5
 cd $ST_DIR/cgedf
-sleep `expr 5 + $3/1000` | st-trace-schedule cgedf_m$1_p$2_d$3_$4_u$5 &
+sleep $gap | st-trace-schedule cgedf_m$1_p$2_d$3_$4_u$5 &
 release_ts &
 wait
 ./st.sh cgedf_m$1_p$2_d$3_$4_u$5
@@ -42,7 +45,7 @@ cd $EXP_DIR
 ./global_test_hc.sh &
 sleep 5
 cd $ST_DIR/hc_cgedf
-sleep `expr 5 + $3/1000` | st-trace-schedule hc_cgedf_m$1_p$2_d$3_$4_u$5 &
+sleep $gap | st-trace-schedule hc_cgedf_m$1_p$2_d$3_$4_u$5 &
 release_ts &
 wait
 ./st.sh hc_cgedf_m$1_p$2_d$3_$4_u$5
