02b588bbc8b2a0cfdf6084059fe0f82ccfb36ff3
Zewei Chen <czwking1991@gmail.com>
2020-03-20 23:15:41 -0400

    --

--- litmus-scripts/ft_test.sh
+++ litmus-scripts/ft_test.sh
@@ -6,44 +6,71 @@ ST_DIR=$EXP_DIR/st
 
 cd $EXP_DIR
 
-python task_gen_vary_utilization.py $1 $2 $3 $5
+python task_gen_vary_utilization.py $1 $2 $3 $4 $5
+
+chmod +x global_$1_$2_$3_$4_$5.sh
 
 cd $FT_DIR
 cd $FT_DIR/gedf
 ./clean.sh
 setsched GSN-EDF
 cd $EXP_DIR
-./global_test.sh &
+./global_$1_$2_$3_$4_$5.sh &
 sleep 5
 cd $FT_DIR/gedf
-sleep `expr 5 + $3` | ft-trace-overheads gedf_m$1_p$2_d$3_$4_u$5 &
+sleep `expr 5 + $3` | ft-trace-overheads gedf_$1_$2_$3_$4_$5 &
 release_ts &
 wait
-./ft.sh gedf_m$1_p$2_d$3_$4_u$5
+./ft.sh gedf_$1_$2_$3_$4_$5
 
 cd $FT_DIR/cgedf
 ./clean.sh
 setsched CG-EDF
 cd $EXP_DIR
-./global_test.sh &
+./global_$1_$2_$3_$4_$5.sh &
 sleep 5
 cd $FT_DIR/cgedf
 sleep `expr 5 + $3` | ft-trace-overheads cgedf_m$1_p$2_d$3_$4_u$5 &
 release_ts &
 wait
-./ft.sh cgedf_m$1_p$2_d$3_$4_u$5
+./ft.sh cgedf_$1_$2_$3_$4_$5
 
-cd $FT_DIR/hc_cgedf
+cd $FT_DIR
+cd $FT_DIR/gfp
 ./clean.sh
-setsched CG-EDF
+setsched GSN-FP
 cd $EXP_DIR
-./global_test.sh &
+./global_$1_$2_$3_$4_$5.sh &
 sleep 5
-cd $FT_DIR/hc_cgedf
-sleep `expr 5 + $3` | ft-trace-overheads hc_cgedf_m$1_p$2_d$3_$4_u$5 &
+cd $FT_DIR/gfp
+sleep `expr 5 + $3` | ft-trace-overheads gfp_$1_$2_$3_$4_$5 &
 release_ts &
 wait
-./ft.sh hc_cgedf_m$1_p$2_d$3_$4_u$5
+./ft.sh gfp_$1_$2_$3_$4_$5
+
+cd $FT_DIR/cgfp
+./clean.sh
+setsched CG-FP
+cd $EXP_DIR
+./global_$1_$2_$3_$4_$5.sh &
+sleep 5
+cd $FT_DIR/cgfp
+sleep `expr 5 + $3` | ft-trace-overheads cgfp_$1_$2_$3_$4_$5 &
+release_ts &
+wait
+./ft.sh cgfp_$1_$2_$3_$4_$5
+
+# cd $FT_DIR/hc_cgedf
+# ./clean.sh
+# setsched CG-EDF
+# cd $EXP_DIR
+# ./global_test.sh &
+# sleep 5
+# cd $FT_DIR/hc_cgedf
+# sleep `expr 5 + $3` | ft-trace-overheads hc_cgedf_m$1_p$2_d$3_$4_u$5 &
+# release_ts &
+# wait
+# ./ft.sh hc_cgedf_m$1_p$2_d$3_$4_u$5
 
 # cd $FT_DIR/pedf
 # ./clean.sh
--- litmus-scripts/scheduling_test.sh
+++ litmus-scripts/scheduling_test.sh
@@ -6,7 +6,7 @@ ST_DIR=$EXP_DIR/st
 
 cd $EXP_DIR
 
-python task_gen_vary_utilization.py $1 $2 $3 $5
+# python task_gen_vary_utilization.py $1 $2 $3 $5
 gap=`expr 5 + $3 / 1000`
 
 cd $ST_DIR
@@ -14,7 +14,7 @@ cd $ST_DIR/gedf
 ./clean.sh
 setsched GSN-EDF
 cd $EXP_DIR
-./global_test.sh &
+./global_${1}_${2}_${3}_${4}_${5}.sh  &
 sleep 2
 cd $ST_DIR/gedf
 sleep $gap | st-trace-schedule gedf_m$1_p$2_d$3_u$5 &
@@ -27,7 +27,7 @@ cd $ST_DIR/cgedf
 ./clean.sh
 setsched CG-EDF
 cd $EXP_DIR
-./global_test.sh &
+./global_${1}_${2}_${3}_${4}_${5}.sh &
 sleep 2
 cd $ST_DIR/cgedf
 sleep $gap | st-trace-schedule cgedf_m$1_p$2_d$3_u$5 &
@@ -36,16 +36,16 @@ wait
 # ./st.sh cgedf_m$1_p$2_d$3_i$4_u$5
 st-job-stats -S *.bin >> result_m$1_p$2_d$3_u$5.txt
 
-cd $ST_DIR/hc_cgedf
-./clean.sh
-setsched CG-EDF
-cd $EXP_DIR
-./global_test_hc.sh &
-sleep 2
-cd $ST_DIR/hc_cgedf
-sleep $gap | st-trace-schedule hc_cgedf_m$1_p$2_d$3_u$5 &
-release_ts &
-wait
-# ./st.sh hc_cgedf_m$1_p$2_d$3_i$4_u$5
-st-job-stats -S *.bin >> result_m$1_p$2_d$3_u$5.txt
+# cd $ST_DIR/hc_cgedf
+# ./clean.sh
+# setsched CG-EDF
+# cd $EXP_DIR
+# ./global_test_hc.sh &
+# sleep 2
+# cd $ST_DIR/hc_cgedf
+# sleep $gap | st-trace-schedule hc_cgedf_m$1_p$2_d$3_u$5 &
+# release_ts &
+# wait
+# # ./st.sh hc_cgedf_m$1_p$2_d$3_i$4_u$5
+# st-job-stats -S *.bin >> result_m$1_p$2_d$3_u$5.txt
 
