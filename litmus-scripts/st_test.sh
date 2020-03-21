9880f9136e92c7788b8d5bc6c68cb1f3acd09104
Zewei Chen <czwking1991@gmail.com>
2020-03-21 11:37:16 -0400

    --

--- litmus-scripts/st_test.sh
+++ litmus-scripts/st_test.sh
@@ -6,7 +6,8 @@ ST_DIR=$EXP_DIR/st
 
 cd $EXP_DIR
 
-python task_gen_vary_utilization.py $1 $2 $3 $5
+python task_gen_vary_utilization.py $1 $2 $3 $4 $5
+# python task_gen_vary_utilization_random_period.py $1 $2 $3 $4 $5
 
 cd $ST_DIR
 cd $ST_DIR/gedf
@@ -33,15 +34,15 @@ release_ts &
 wait
 ./st.sh cgedf_m$1_p$2_d$3_$4_u$5
 
-cd $ST_DIR/hc_cgedf
-./clean.sh
-setsched CG-EDF
-cd $EXP_DIR
-./global_test_hc.sh &
-sleep 5
-cd $ST_DIR/hc_cgedf
-sleep `expr 5 + $3` | st-trace-schedule hc_cgedf_m$1_p$2_d$3_$4_u$5 &
-release_ts &
-wait
-./st.sh hc_cgedf_m$1_p$2_d$3_$4_u$5
+# cd $ST_DIR/hc_cgedf
+# ./clean.sh
+# setsched CG-EDF
+# cd $EXP_DIR
+# ./global_test_hc.sh &
+# sleep 5
+# cd $ST_DIR/hc_cgedf
+# sleep `expr 5 + $3` | st-trace-schedule hc_cgedf_m$1_p$2_d$3_$4_u$5 &
+# release_ts &
+# wait
+# ./st.sh hc_cgedf_m$1_p$2_d$3_$4_u$5
 
