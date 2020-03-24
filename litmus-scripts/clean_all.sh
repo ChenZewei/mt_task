9e81e9fc6ba1f2b43a9497c0ef05c7a65f8d712d
Zewei Chen <czwking1991@gmail.com>
2020-03-24 14:13:18 -0400

    --

--- litmus-scripts/clean_all.sh
+++ litmus-scripts/clean_all.sh
@@ -1,9 +1,15 @@
+EXP_DIR=/home/ubuntu/exp
+FT_DIR=$EXP_DIR/ft
+ST_DIR=$EXP_DIR/st
+
+cd $ST_DIR
+
 for dir in `ls`
 do
   if [ -d $dir ]; then
     echo "enter $dir"
     cd $dir
-    rm *.bin *.float32 *.sf32 *.log *.csv *.pdf
+    rm *.txt *.bin *.float32 *.sf32 *.log *.csv *.pdf
     echo "exit $dir"
     cd ../
   fi
