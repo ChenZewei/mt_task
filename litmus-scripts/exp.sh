9d7d11b5e733545ce9ea49a83e70304e4e518362
Zewei Chen <czwking1991@gmail.com>
2020-03-24 09:41:30 -0400

    --

--- litmus-scripts/exp.sh
+++ litmus-scripts/exp.sh
@@ -13,7 +13,7 @@ duration=$3
 iteration=$4
 
 
-for u in $(seq 0.05 0.05 1)
+for u in $(seq 0.1 0.1 1)
 do
   # ./exp.sh $1 1 $2 $iteration $u
   #./exp.sh $1 4 $2 $iteration $u 
@@ -21,26 +21,26 @@ do
   ./st_test.sh $m $p $duration $iteration $u
 done
 
-cd $ST_DIR/gedf
-rm gedf_ratio_m$1_p$2_d$3.txt
+# cd $ST_DIR/gedf
+# rm gedf_ratio_m$1_p$2_d$3.txt
 
-cd $ST_DIR/cgedf
-rm cgedf_ratio_m$1_p$2_d$3.txt
+# cd $ST_DIR/cgedf
+# rm cgedf_ratio_m$1_p$2_d$3.txt
 
 # cd $ST_DIR/hc_cgedf
 # rm hc_cgedf_ratio_m$1_p$2_d$3.txt
 
-for u in $(seq 0.05 0.05 1)
-do
-  cd $ST_DIR/gedf
-  s=`grep "1" result_m$1_p$2_d$3_u$u.txt| wc -l`
-  echo "scale=3;$s/$iteration" | bc >> gedf_ratio_m$1_p$2_d$3.txt
+# for u in $(seq 0.1 0.1 1)
+# do
+#   cd $ST_DIR/gedf
+#   s=`grep "1" result_m$1_p$2_d$3_u$u.txt| wc -l`
+#   echo "scale=3;$s/$iteration" | bc >> gedf_ratio_m$1_p$2_d$3.txt
 
-  cd $ST_DIR/cgedf
-  s=`grep "1" result_m$1_p$2_d$3_u$u.txt| wc -l`
-  echo "scale=3;$s/$iteration" | bc >> cgedf_ratio_m$1_p$2_d$3.txt
+#   cd $ST_DIR/cgedf
+#   s=`grep "1" result_m$1_p$2_d$3_u$u.txt| wc -l`
+#   echo "scale=3;$s/$iteration" | bc >> cgedf_ratio_m$1_p$2_d$3.txt
 
-  # cd $ST_DIR/hc_cgedf
-  # s=`grep "1" result_m$1_p$2_d$3_u$u.txt| wc -l`
-  # echo "scale=3;$s/$iteration" | bc >> hc_cgedf_ratio_m$1_p$2_d$3.txt
-done
+#   # cd $ST_DIR/hc_cgedf
+#   # s=`grep "1" result_m$1_p$2_d$3_u$u.txt| wc -l`
+#   # echo "scale=3;$s/$iteration" | bc >> hc_cgedf_ratio_m$1_p$2_d$3.txt
+# done
