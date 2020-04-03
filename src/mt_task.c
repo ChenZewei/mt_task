ac2b6f9232cc40b3507828b3f1c21b4efb2acc04
Zewei Chen <czwking1991@gmail.com>
2020-04-03 11:25:58 -0400

    --

--- src/mt_task.c
+++ src/mt_task.c
@@ -90,7 +90,7 @@ static int loop_us(double us);
 static int loop_ns(double ns);
 
 // #define loop_once() loop(1)
-#define loop_once() loop_ns(10)
+#define loop_once() loop_us(10)
 
 int ceil(double num) {
 	int result = num;
