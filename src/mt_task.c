61c46ea2ab1937d9f4d91842b89561b14a41265c
Zewei Chen <czwking1991@gmail.com>
2020-04-03 11:03:35 -0400

    --

--- src/mt_task.c
+++ src/mt_task.c
@@ -378,10 +378,12 @@ static int loop_ms(double ms) {
 		loop_start = now;
 		tmp += loop_once();
 		now = cputime();
+		if (ms/1000 < (now-start)) {
+			printf("Actually execute for %8.2f ms (suppose to be %8.2f ms)\n", (now-start)*1000, ms);
+			printf("Max loop: %8.2f ms, this loop %8.2f ms.\n", max_loop, (now-loop_start)*1000);
+		}
 		if (max_loop < now - loop_start)
 			max_loop = now - loop_start;
-		if (ms/1000 < (now-start))
-			printf("actually execute for %8.2f ms (suppose to be %8.2f ms)\n", (now-start)*1000, ms);
 	}
 	return tmp;
 	// long iteration = ms * 267000;
